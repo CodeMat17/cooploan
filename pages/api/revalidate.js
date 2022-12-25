a2dfed4afc59c530240ef17a476f84e8399bc9eb
CodeMat <codemat.biz@gmail.com>
2022-12-25 09:48:16 +0100

    updated

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,3 +1,5 @@
+// revalidate handler
+
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
